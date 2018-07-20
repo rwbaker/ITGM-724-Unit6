45c036162680b43484ee52ba99268db06da4177c
Richard Baker <rwbaker@ge.com>
2018-07-19 22:04:36 -0700

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Exercise 2, Unit 5-6, ITGM-727
+ * @author Richard Baker, rbaker22@student.scad.edu
+ *
+ * @assignment
+ *
+ *
+ *
+ * @short desc
+ *
+ *
+ *
+ * @dependencies
+ * Bootstrap @ https://getbootstrap.com
+ *
+ */
+?>
+
+  <!DOCTYPE html>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="bootstrap-4/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style/main.css">
+  <title>Exercise 2, ITGM-727</title>
+  </head>
+
+  <body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="bootstrap-4/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap-4/js/bootstrap.bundle.min.js"></script>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+
+          ...
+
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+    </div>
+
+
+  </body>
+
+  </html>
